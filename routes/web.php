e95b03710b59d5bceab564331cac446ef83d2c70
bob <mhragab@SPA.GOV.SA>
2023-01-17 10:41:19 +0300

    redirect home to chalets list

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\ChaletsController@index')->name('home');
 
 
 Route::resource('chalet',ChaletsController::class);
